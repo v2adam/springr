ffe6f6be8c71fa51d75d2b1e5660a255f9467fc0
v2adam <adam.vig10@gmail.com>
2018-03-09 13:37:31 +0100

    fix

--- src/main/js/src/scenes/Page10/index.jsx
+++ src/main/js/src/scenes/Page10/index.jsx
@@ -29,7 +29,7 @@ class Page10 extends Component {
             fileList: [],
             header: [],
             rows: [],
-            beforeEditRows: [],
+            changedRows: [],
             columns: [{
                 dataIndex: 'id',
                 title: 'id',
